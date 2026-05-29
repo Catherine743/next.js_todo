2003c6b9fe1543a91b9fec245595af59d0b697e8
catherine <catherinepgeorge02@gmail.com>
2026-05-29 10:31:53 +0530

    Added code in github

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 "use client"
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
   const [noteData, setNoteData] = useState({
     title: "", description: ""
   });
+
+  const [allNotes, setAllNotes] = useState([]);
+
+  useEffect(() => {
+    getNotes()
+  }, [])
 
   const handleAddNote = () => {
     const { title, description } = noteData
@@ -22,6 +28,13 @@ export default function Home() {
       console.log(response)
     }
   }
+
+  const getNotes = async () => {
+    const response = await fetch("/api/notes")
+    setAllNotes(await response.json())
+  }
+  console.log(allNotes)
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <div className="bg-gray-300 block max-w-sm m-5 p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
@@ -36,14 +49,16 @@ export default function Home() {
       </div>
 
       {/* display area */}
-      <div className="bg-gray-300 block max-w-sm m-5 p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
-        <h2 className="text-2xl text-blue-600 mb-3 tracking-tight text-heading leading-8">Your Notes:</h2>
-        <p className="text-body text-black">Here are the biggest technology acquisitions of 2025 so far, in reverse chronological order.</p>
-      </div>
-      <div className="flex gap-2">
-        <button className="bg-blue-500 p-2 text-white">Edit</button>
-        <button className="bg-red-500 p-2 text-white">Delete</button>
-      </div>
+      {allNotes?.length > 0 ? allNotes.map((notes: any, index: number) => (
+        <div key={index} className="bg-gray-300 block max-w-sm m-5 p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
+          <h2 className="text-2xl text-blue-600 mb-3 tracking-tight text-heading leading-8">{notes.title}</h2>
+          <p className="text-body text-black">{notes.description}</p>
+          <div className="flex gap-2">
+            <button className="bg-blue-500 p-2 text-white">Edit</button>
+            <button className="bg-red-500 p-2 text-white">Delete</button>
+          </div>
+        </div>
+      )) : <p>Nothing to display</p>}
     </div>
   );
 }
